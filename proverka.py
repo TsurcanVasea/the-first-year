be2590759ba997a5874255d1712596e019c62387
Василий <turcanvasea1@gmail.com>
2023-09-27 22:43:24 +0300

    Импортирован by time Добавление функции sleep

--- proverka.py
+++ proverka.py
@@ -1,3 +1,6 @@
+import time
+
+
 sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
 sides = sorted(sides, reverse=True)
 smax = 0
@@ -13,4 +16,4 @@ for i in range(len(sides)):
                     if s > smax:
                         smax = s
 print("Максимальная площадь треугольника", smax)
-
+time.sleep(60)
